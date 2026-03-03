f3f1ab75ba42abfd7bc101adb9b4ddf8c010a386
Riya-Vaghani <f20240109@hyderabad.bits-pilani.ac.in>
2026-03-03 21:37:08 +0530

    Create Admin.jsx

---
+++ frontend/src/pages/Admin.jsx
@@ -0,0 +1,17 @@
+function Admin() {
+  return (
+    <div className="container">
+      <h1>Admin Chamber</h1>
+
+      <div className="card">
+        <button>Add Movie</button>
+        <button>Edit Movie</button>
+        <button>Delete Movie</button>
+        <button>Add Show</button>
+        <button>Update Pricing</button>
+      </div>
+    </div>
+  );
+}
+
+export default Admin;
