ebc1827983cfbde7a9d30df5be78e7383d2c026e
Riya-Vaghani <f20240109@hyderabad.bits-pilani.ac.in>
2026-03-03 21:51:40 +0530

    Create Shows.jsx

---
+++ frontend/src/pages/Shows.jsx
@@ -0,0 +1,11 @@
+function Shows() {
+  return (
+    <div className="page">
+      <h2>Shows</h2>
+      <div className="card">Movie: The Emerald Throne</div>
+      <div className="card">Time: 7:00 PM</div>
+    </div>
+  );
+}
+
+export default Shows;
