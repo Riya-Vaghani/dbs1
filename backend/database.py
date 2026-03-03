3fd5f62ff4cd5faf443fe134e80841627bd30999
Riya-Vaghani <f20240109@hyderabad.bits-pilani.ac.in>
2026-03-03 20:58:39 +0530

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -1,10 +1,5 @@
+import os
 import psycopg
 
 def get_connection():
-    return psycopg.connect(
-        dbname="online_movie_booking",
-        user="postgres",
-        password="1234",
-        host="localhost",
-        port="5432"
-    )
+    return psycopg.connect(os.environ["DATABASE_URL"])
