f1c10850bc42d4b0bd69245bdbd5b8218f5ebc08
Riya-Vaghani <f20240109@hyderabad.bits-pilani.ac.in>
2026-03-03 21:28:55 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,30 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import MovieDetails from "./pages/MovieDetails";
+import Shows from "./pages/Shows";
+import Seats from "./pages/Seats";
+import Booking from "./pages/Booking";
+import Admin from "./pages/Admin";
+import "./App.css";
 
 function App() {
   return (
-    <div className="min-h-screen bg-gray-100">
+    <div>
+      <nav className="navbar">
+        <h2>Online Movie Booking</h2>
+        <div>
+          <a href="/">Home</a>
+          <a href="/admin">Admin</a>
+        </div>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movie" element={<MovieDetails />} />
+        <Route path="/shows" element={<Shows />} />
+        <Route path="/seats" element={<Seats />} />
+        <Route path="/booking" element={<Booking />} />
+        <Route path="/admin" element={<Admin />} />
       </Routes>
     </div>
   );
