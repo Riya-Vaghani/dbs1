eabcc573ed7141d5773ebfc3feb95dab335c5470
Riya-Vaghani <f20240109@hyderabad.bits-pilani.ac.in>
2026-03-03 21:34:07 +0530

    Refactor App component routes and structure

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,17 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
-import MovieDetails from "./pages/MovieDetails";
+import Movies from "./pages/Movies";
 import Shows from "./pages/Shows";
 import Seats from "./pages/Seats";
 import Booking from "./pages/Booking";
 import Admin from "./pages/Admin";
-import "./App.css";
 
 function App() {
   return (
-    <div>
-      <nav className="navbar">
-        <h2>Online Movie Booking</h2>
-        <div>
-          <a href="/">Home</a>
-          <a href="/admin">Admin</a>
-        </div>
-      </nav>
-
+    <div className="app">
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/movie" element={<MovieDetails />} />
+        <Route path="/movies" element={<Movies />} />
         <Route path="/shows" element={<Shows />} />
         <Route path="/seats" element={<Seats />} />
         <Route path="/booking" element={<Booking />} />
