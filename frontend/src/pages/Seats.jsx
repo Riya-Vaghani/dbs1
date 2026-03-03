111706647972e7aae0463994e7fcaa332449b82c
Riya-Vaghani <f20240109@hyderabad.bits-pilani.ac.in>
2026-03-03 21:51:53 +0530

    Create Seats.jsx

---
+++ frontend/src/pages/Seats.jsx
@@ -0,0 +1,11 @@
+function Seats() {
+  return (
+    <div className="page">
+      <h2>Seats</h2>
+      <div className="card">Available Seats: 120</div>
+      <button>Reserve Seat</button>
+    </div>
+  );
+}
+
+export default Seats;
